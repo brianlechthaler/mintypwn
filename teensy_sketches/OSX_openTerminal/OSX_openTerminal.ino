dcbfe7ad8fbf17345feb054b8dd1f94f17b03eae
Brian Lechthaler <brianlechthaler@gmail.com>
2015-10-04 15:38:48 -0700

    cleaned up folders and added OSX_openTerminal
    folder cleanup&& added script that opens up terminal

---
+++ teensy_sketches/OSX_openTerminal/OSX_openTerminal.ino
@@ -0,0 +1,13 @@
+void setup() {
+  delay(5000);
+  //wait 5s before doing anything
+  Keyboard.set_modifier(MODIFIERKEY_GUI);
+  Keyboard.set_key1(KEY_SPACE);
+  Keyboard.send_now();
+  
+}
+
+void loop() {
+  
+
+}
